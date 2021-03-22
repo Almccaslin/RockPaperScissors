92591f2b61be89767b8d027b25f76a431a4e41e5
Almccaslin <mccaslinallison2003@gmail.com>
2021-03-22 01:12:59 +0000

    Added a while loop

--- main.py
+++ main.py
@@ -1,55 +1,64 @@
 import random
-x = random.randint(1, 3)
 
 c = 0
+y = "a"
 
 player = input(str("Name: "))
-rps = input("Rock, Paper, Scissors")
-
 p = open(player, "a")
 
-if (x == 1):
-    print("Computer: ROCK")
-if (x == 2):
-    print("Computer: PAPER")
-if (x == 3):
-    print("Computer: SCISSORS")
 
-y = rps.upper()
-print("Human: " + y)
+while (y != "STOP"):
+    c = 0
+    p = open(player, "a")
+    rps = input("Rock, Paper, Scissors: ")
+    x = random.randint(1, 3)
+    if (x == 1):
+        print("Computer: ROCK")
+    if (x == 2):
+        print("Computer: PAPER")
+    if (x == 3):
+        print("Computer: SCISSORS")
 
-if (x == 1 and y == "ROCK"):
-    c = 1
-if (x == 1 and y == "PAPER"):
-    c = 2
-if (x == 1 and y == "SCISSORS"):
-    c = 3
+    y = rps.upper()
+    print("Human: " + y)
 
-if (x == 2 and y == "PAPER"):
-    c = 1
-if (x == 2 and y == "SCISSORS"):
-    c = 2
-if (x == 2 and y == "ROCK"):
-    c = 3
+    if (x == 1 and y == "ROCK"):
+        c = 1
+    if (x == 1 and y == "PAPER"):
+        c = 2
+    if (x == 1 and y == "SCISSORS"):
+        c = 3
 
-if (x == 3 and y == "SCISSORS"):
-    c = 1
-if (x == 3 and y == "ROCK"):
-    c = 2
-if (x == 3 and y == "PAPER"):
-    c = 3
+    if (x == 2 and y == "PAPER"):
+        c = 1
+    if (x == 2 and y == "SCISSORS"):
+        c = 2
+    if (x == 2 and y == "ROCK"):
+        c = 3
 
-if (c == 1):
-    print("It's a tie.")
-    p.write("\nIt's a tie.\n")
-elif (c == 2):
-    print("You win.")
-    p.write("\nYou win.\n")
-elif (c == 3):
-    print("You lose.")
-    p.write("\nYou lose.\n")
-else:
-    print("Invalid Respose")
-    p.write("\nInvalid respose\n")
+    if (x == 3 and y == "SCISSORS"):
+        c = 1
+    if (x == 3 and y == "ROCK"):
+        c = 2
+    if (x == 3 and y == "PAPER"):
+        c = 3
+
+    if (c == 1):
+        w = ("It's a tie.")
+
+    elif (c == 2):
+        w = ("You win.")
+
+    elif (c == 3):
+        w = ("You lose.")
+
+    else:
+        w = ("Invalid Response")
+
+    print(w)
+    y = (input("STOP to end: "))
+
+    p.write(w)
+    p.write("\n")
 
 p.close()
